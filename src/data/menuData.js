990d40dd8e3ecf04f7e08fc5ecc5590e7ea4f22a
wangzhedong <592891112@qq.com>
2019-07-01 23:39:59 +0800

    代码提交

--- src/data/menuData.js
+++ src/data/menuData.js
@@ -2,20 +2,11 @@ const menus = [
     { id: '0', label: '首页', path: 'home', icon: 'fa fa-dashboard' },
     { id: '1', label: '用户管理', path: 'users', icon: 'fa fa-users' },
     { id: '2', label: '权限管理', path: 'roles', icon: 'fa fa-square' },
-    { id: '3', label: '资源管理', path: 'menus', icon: 'fa fa-bars' },
-    { id: '4', label: '模块管理', path: 'modules', icon: 'fa fa-cubes' },
-    { id: '5', label: '部门管理', path: 'depts', icon: 'fa fa-address-card' },
-    { id: '6', label: '规则管理', path: 'rules', icon: 'fa fa-sitemap' },
-    {
-        id: '7',
-        icon: 'fa fa-bar-chart',
-        label: '报表管理',
-        submenu: [
-            { id: 'tplmg', name: 'tplmg', label: '模板管理', icon: 'fa fa-info' },
-            { id: 'tplcheck', name: 'tplcheck', label: '报表查看', icon: 'fa fa-info' },
-            { id: 'tplsum', name: 'tplsum', label: '报表汇总', icon: 'fa fa-info' }
-        ]
-    },
+    { id: '3', label: '模块管理', path: 'modules', icon: 'fa fa-cubes' },
+    { id: '4', label: '部门管理', path: 'depts', icon: 'fa fa-address-card' },
+    { id: '5', label: '规则管理', path: 'rules', icon: 'fa fa-sitemap' },
+    { id: '6', label: '报表管理', path: 'reportmg', icon: 'fa fa-bar-chart' },
+    { id: '7', label: '报表填写', path: 'reportfill', icon: 'fa fa-pencil' },
     { id: '8', label: '设置', path: 'setting', icon: 'fa fa-cogs' }
 ]
 export {
--- src/pages/depts.vue
+++ src/pages/depts.vue
@@ -1,23 +1,41 @@
 <template>
-    <div>
-        <el-row>
-            <el-button icon="el-icon-plus" type="primary" @click="openAdd">新增</el-button>
-        </el-row>
-    </div>
+  <div>
+    <el-row>
+      <el-button icon="el-icon-plus" type="primary" @click="openAdd">新增</el-button>
+    </el-row>
+    <el-table
+      height="350"
+      :data="tableData"
+      style="width: 100%;"
+    >
+      <el-table-column prop="deptName" label="部门名称"></el-table-column>
+      <el-table-column prop="uemail" label="部门编号"></el-table-column>
+      <el-table-column fixed="right" label="操作" width="360px">
+        <template slot-scope="scope">
+          <el-button type="text" @click="editDept(scope.row)" size="small">修改部门信息</el-button>
+          <el-button type="text" @click="delDept(scope.row)" size="small">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 <script>
 export default {
-    name:'depts',
-    data(){
-
+    name: 'depts',
+    data() {
+        return {
+            tableData:[]
+        }
     },
-    methods:{
-        openAdd(){
+    methods: {
+        openAdd() {},
+        addOrUpdate() {},
+        editDept(deptData){
 
         },
-        addOrUpdate(){
-
+        delDept(deptData){
+            
         }
-    }
-}
+    },
+};
 </script>
